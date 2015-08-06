7253ef9c297e74635e9358f9dd1dffb9d2b13f67
Tim Kuijsten <tim@netsend.nl>
2015-08-07 01:18:57 +0200

    prepare for english list

--- passphrase.js
+++ passphrase.js
@@ -27,5 +27,5 @@ run();
 var bitsPerWord = Math.log2(list.length);
 var bits = bitsPerWord * nrWords;
 
-document.getElementById('wordkeyspace').appendChild(document.createTextNode(list.length + ' (' + Math.round(bitsPerWord) + ' bit)'));
-document.getElementById('phrasekeyspace').appendChild(document.createTextNode('ruim ' + Math.floor(bits) + ' bit'));
+document.getElementById('wordkeyspace').appendChild(document.createTextNode(list.length + ' (~' + Math.round(bitsPerWord) + ' bit)'));
+document.getElementById('phrasekeyspace').appendChild(document.createTextNode('>' + Math.floor(bits) + ' bit'));
